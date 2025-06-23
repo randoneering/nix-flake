32ea594a73fb722afff11539f35de1b866e8d9bd
randoneering <justin@randoneering.tech>
2025-06-22 21:39:55 -0600

    updating

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,9 +1,19 @@
 {pkgs, ...}: {
   home.packages = [pkgs.gh];
 
+  home.file.".ssh/allowed_signers".text =
+    "* ${builtins.readFile /home/randoneering/.ssh/l15.pub}";
+
   programs.git = {
     enable = true;
     userName = "randoneering";
     userEmail = "justin@randoneering.tech";
+    extraConfig = {
+      # Sign all commits using ssh key
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+      gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+      user.signingkey = "~/.ssh/l15.pub";
+  };
   };
 }
