ac6649a419df450d0c5b9e26f31e4836e2e0e894
randoneering <justin@randoneering.tech>
2025-07-05 16:54:34 -0600

    adding amd gpu settings for L15

--- hosts/L15/default.nix
+++ hosts/L15/default.nix
@@ -14,6 +14,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # AMD Drivers
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "amdgpu" ];
   # Flox Settings
   nix.settings.trusted-substituters = [ "https://cache.flox.dev" ];
   nix.settings.trusted-public-keys = [ "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs=" ];
