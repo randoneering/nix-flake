93da25f7e0cc9118b6055a01f504ff57457f3b5b
randoneering <justin@randoneering.tech>
2025-09-05 21:21:33 -0600

    popping and locking

--- home/shell/terminals.nix
+++ home/shell/terminals.nix
@@ -1,21 +1,10 @@
 {pkgs, ...}:{
 
-programs.kitty = {
-    enable = true;
-    themeFile= "Dracula";
-    settings = {
-      confirm_os_window_close = 0;
-      dynamic_background_opacity = true;
-      enable_audio_bell = false;
-      mouse_hide_wait = "-1.0";
-      window_padding_width = 10;
-    };
-};
 
 programs.ghostty = {
   enable = true;
   settings = {
-    theme = "Dracula";
+    theme = "Popping and Locking";
     shell-integration-features = "sudo";
   };
   enableFishIntegration = true;
