e2fe87a4d3b08853115922cb41c7491f1ed850eb
randoneering <justin@randoneering.tech>
2025-09-20 12:55:18 -0600

    update with nix-wks details

--- hosts/wks/default.nix
+++ hosts/wks/default.nix
@@ -11,15 +11,27 @@
       ./hardware-configuration.nix
     ];
 
+  # Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/nvme0n1";
+  boot.loader.grub.useOSProber = true;
+  boot.initrd.luks.devices."luks-25ac66f8-28c2-4b24-9f65-2aa0a8f0ad2c".device = "/dev/disk/by-uuid/25ac66f8-28c2-4b24-9f65-2aa0a8f0ad2c";
+
+  # Setup keyfile
+  boot.initrd.secrets = {
+    "/boot/crypto_keyfile.bin" = null;
+  };
+  boot.initrd.luks.devices."luks-38a8795e-d976-4853-96c2-30836c1c1106".keyFile = "/boot/crypto_keyfile.bin";
+  boot.initrd.luks.devices."luks-25ac66f8-28c2-4b24-9f65-2aa0a8f0ad2c".keyFile = "/boot/crypto_keyfile.bin";
 
   # Flox Settings
   nix.settings.trusted-substituters = [ "https://cache.flox.dev" ];
   nix.settings.trusted-public-keys = [ "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs=" ];
 
-  boot.initrd.luks.devices."luks-6c7efb18-8b6a-4e45-ae5f-f5f35162f86b".device = "/dev/disk/by-uuid/6c7efb18-8b6a-4e45-ae5f-f5f35162f86b";
+  # Networking
   networking.hostName = "nix-wks";
   networking.networkmanager.enable = true;
 
@@ -36,11 +48,11 @@
     fsType = "nfs";
     options = ["x-systemd.automount" "noauto" "x-systemd.after=network-online.target" "x-systemd.mount-timeout=5s"];
   };
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
 
   # Enable Bluetooth
   hardware.bluetooth.enable = true;
-  system.stateVersion = "25.05"; # Did you read the comment?
+
+  system.stateVersion = "25.05";
 
 }
---
+++ hosts/wks/hardware-configuration.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/79a5d02f-0071-4d9a-a16d-562896b7fb23";
+      fsType = "ext4";
+    };
+
+  boot.initrd.luks.devices."luks-38a8795e-d976-4853-96c2-30836c1c1106".device = "/dev/disk/by-uuid/38a8795e-d976-4853-96c2-30836c1c1106";
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/82b9c7e2-cd2b-43f7-9d55-d42de5afb61a"; }
+    ];
+
+
+  networking.useDHCP = lib.mkDefault true;
+
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
