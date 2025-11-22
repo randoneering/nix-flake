81c978d672efd2b1ae326af1b52ac6b9c2f415ad
randoneering <justin@randoneering.tech>
2025-11-22 13:14:59 -0700

    update to flake.lock and ollama

--- modules/llm/ollama.nix
+++ modules/llm/ollama.nix
@@ -4,20 +4,20 @@
   username,
   ...
 }:{
-  # systemd.services.ollama.serviceConfig = {
-  #   Environment = [ "OLLAMA_HOST=10.10.1.232:11434" ];
-  # };
+  systemd.services.ollama.serviceConfig = {
+    Environment = [ "OLLAMA_HOST=10.10.1.232:11434" ];
+  };
    # Ollama Setup
   services.ollama = {
     enable = true;
     acceleration = "cuda";
-    loadModels = ["llama3.2:3b" "deepseek-r1:1.5b" "gemma3:12b" "qwen3:8b" "qwen3:14b"];
+    loadModels = ["llama3.2:3b" "deepseek-r1:1.5b" "gemma3:12b" "qwen3:8b" "qwen3:14b" "qwen3-coder:latest"];
     openFirewall = true;
     host = "10.10.1.232"; # Make Ollama accesible outside of localhost
   };
   # Open-Webui setup
   services.open-webui = {
-    enable = true;
+    enable = true
     openFirewall = true;
     host = "10.10.1.232";
     environment = {
