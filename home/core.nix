ef81bcfadda5e804fbcbd2ec61143ac1fc0a20d4
randoneering <justin@randoneering.tech>
2025-12-26 22:08:17 -0700

    add bun for opcode

--- home/core.nix
+++ home/core.nix
@@ -3,7 +3,7 @@
     inherit username;
     homeDirectory = "/home/${username}";
 
-    stateVersion = "25.05";
+    stateVersion = "25.11";
   };
 
   programs.home-manager.enable = true;
--- home/programs/common.nix
+++ home/programs/common.nix
@@ -19,6 +19,7 @@
     # rust
     cargo
     rustc
+    bun
 
     # python
     ruff
